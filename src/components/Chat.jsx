ad15c07990cff76134d1c4dfec6f508e29376ed7
Sherin S <62053384+sherinsk@users.noreply.github.com>
2024-07-07 17:55:03 +0530

    added

--- src/components/Chat.jsx
+++ src/components/Chat.jsx
@@ -1,7 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import io from 'socket.io-client';
 import axios from 'axios';
-import jwt from 'jsonwebtoken';
 
 const socket = io('https://chat-backend-9pci.onrender.com'); // Backend URL
 
